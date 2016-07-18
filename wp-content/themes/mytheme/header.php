1627f7b42f819f830772084be7d9eac858f0e857
Philipp Havrilla <philipp.havrilla@bluewin.ch>
2016-07-18 21:25:21 +0200

    Replaced HTML with PHP functions

--- wp-content/themes/mytheme/header.php
+++ wp-content/themes/mytheme/header.php
@@ -8,22 +8,23 @@
 <!DOCTYPE html>
 <html <?php language_attributes(); ?>>
     <head>
-        <meta charset="<?php bloginfo("charset"); ?>">
+        <meta charset="<?php bloginfo( 'charset' ); ?>">
         <meta http-equiv="X-UA-Compatible" content="IE=edge">
         <meta name="viewport" content="width=device-width, initial-scale=1">
         <!-- The above 3 meta tags *must* come first in the head; any other head content must come *after* these tags -->
-        <meta name="description" content="">
+        <meta name="description" content="<?php bloginfo( 'description' ); ?>">
         <meta name="author" content="">
 
-        <title>Bootstrap 101 Template</title>
+        <title><?php bloginfo( 'name' ); ?><?php wp_title(); ?></title>
 
         <!-- Bootstrap core CSS -->
         <!-- Latest compiled and minified CSS -->
         <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css" integrity="sha384-1q8mTJOASx8j1Au+a5WDVnPi2lkFfwwEAa8hDDdjZlpLegxhjVME1fgjWPGmkzs7" crossorigin="anonymous">
 
         <!-- Custom styles for this template -->
-        <link rel="stylesheet" href="<?php bloginfo("stylesheet_url"); ?>">
-        <link rel="pingback" href="<?php bloginfo("pingback_url"); ?>">
+        <link rel="stylesheet" href="<?php bloginfo( 'stylesheet_url' ); ?>">
+
+        <link rel="pingback" href="<?php bloginfo( 'pingback_url' ); ?>">
 
         <!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
         <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
