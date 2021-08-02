0ddd4869f18ed130f6958501f09f74a1fe2cc176
Philipp Havrilla <philipp.havrilla@bluewin.ch>
2021-08-02 20:30:44 +0200

    Merge POT file into PO files

--- wp-content/themes/mytheme/makepot.sh
+++ wp-content/themes/mytheme/makepot.sh
@@ -10,4 +10,7 @@ if [ ! -w "$file" ] ; then
     exit 1
 fi
 
-find . -iname "*.php" | xargs xgettext -d mytheme -j -o $file -k__ -k_e -k_x --from-code=utf-8 -L PHP
+find . -iname "*.php" | xargs xgettext -d mytheme -o $file -k__ -k_e -k_n -k_x -k_ex -k_nx -kesc_attr__ -kesc_attr_e -kesc_attr_x -kesc_html__ -kesc_html_e -kesc_html_x -k_n_noop -k_nx_noop --from-code=utf-8 -L PHP
+if compgen -G "languages/*.po" > /dev/null ; then
+    msgmerge -U languages/*.po $file --backup=off
+fi
